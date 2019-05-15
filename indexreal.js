b8b31f1555ccb3c3dddc7474f9121a84401f7d20
Faith Goldsberry <faithenglishtutor@gmail.com>
2019-05-14 21:03:14 -0400

    add adverbs of frequency

---
+++ indexreal.js
@@ -0,0 +1,7 @@
+
+var adverbs = ["Never", "Seldom", "Rarely", "Occasionally", "Sometimes", "Often", "Normally", "Regularly", "Usually", "Always"]
+
+function frequency()
+{ var x = document.getElementById("frequency").value;
+  alert(adverbs[x])
+}
